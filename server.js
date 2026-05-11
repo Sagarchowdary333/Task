1569b635ae30ce8319e57ffc4251cff3da710f17
Sagarchowdary3 <sagarchowdary99511@gmail.com>
2026-05-11 13:02:56 +0530

    Task

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
+
     fs.readFile('index.html', (err, data) => {
+
         if (err) {
             res.writeHead(500);
-            res.end("Error loading file");
+            res.end("Error loading page");
             return;
         }
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
         res.write(data);
         res.end();
     });
+
 });
 
 server.listen(PORT, () => {
